57277157d3bb9e5d97cc936a985c23bef6b5a58e
devopsprashu <prashansachincholkar@gmail.com>
2025-08-11 18:15:43 +0530

    code corrected

--- life.tf
+++ life.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features{}
-  subscription_id="fadfa500-48fa-4d7d-ae72-215103d56b2f"
+  subscription_id = "fadfa500-48fa-4d7d-ae72-215103d56b2f"
 }
 
 resource "azurerm_resource_group" "hope" {
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "hope" {
 }
 
 resource "azurerm_storage_account" "hopestg" {
-  name                     = "storageaccountname"
+  name                     = "hopestg"
   resource_group_name      = azurerm_resource_group.hope.name
   location                 = azurerm_resource_group.hope.location
   account_tier             = "Standard"
